3929e09564fb50d52de6c7b79fb1d11fe2de4ccf
Kale Kundert <kale@thekunderts.net>
2016-06-25 12:20:01 -0700

    Test the construct class.

--- tests/test_model.cc
+++ tests/test_model.cc
@@ -1,17 +1,49 @@
+#include <memory>
 #include <catch/catch.hpp>
-#include "model.hh"
+#include <sgrna_design/model.hh>
 
-using namespace sgrna;
+using namespace std;
+using namespace sgrna_design;
 
-TEST_CASE("Test model constructor", "[model]") {
+TEST_CASE("Test domain constructor", "[model]") {
 	Domain domain(
 			"aptamer",
-			"AUACCAGCCGAAAGGCCCUUGGCAG",
+			"GAUACCAGCCGAAAGGCCCUUGGCAGC",
 			ColorEnum::YELLOW,
 			StyleEnum::BOLD);
 
-	REQUIRE(domain.name() == "aptamer");
-	REQUIRE(domain.seq() == "AUACCAGCCGAAAGGCCCUUGGCAG");
-	REQUIRE(domain.color() == ColorEnum::YELLOW);
-	REQUIRE(domain.style() == StyleEnum::BOLD);
+	CHECK(domain.name() == "aptamer");
+	CHECK(domain.seq() == "GAUACCAGCCGAAAGGCCCUUGGCAGC");
+	CHECK(domain.color() == ColorEnum::YELLOW);
+	CHECK(domain.style() == StyleEnum::BOLD);
+}
+
+TEST_CASE("Test the construct class", "[model]") {
+	Construct rna;
+
+	rna += make_shared<Domain>("stem/5", "ACGU");
+	rna += make_shared<Domain>("tetraloop", "GAAA");
+	rna += make_shared<Domain>("stem/3", "ACGU");
+
+	REQUIRE(rna.seq() == "ACGUGAAAACGU");
+
+	REQUIRE(rna["stem/5"]->seq() == "ACGU");
+	REQUIRE(rna["tetraloop"]->seq() == "GAAA");
+	REQUIRE(rna["stem/3"]->seq() == "ACGU");
+
+	REQUIRE(rna.index_5("stem/5") == 0);
+	REQUIRE(rna.index_3("stem/5") == 3);
+	REQUIRE(rna.index_5("tetraloop") == 4);
+	REQUIRE(rna.index_3("tetraloop") == 7);
+	REQUIRE(rna.index_5("stem/3") == 8);
+	REQUIRE(rna.index_3("stem/3") == 11);
+
+	ConstructPtr rna_copy = rna.copy();
+
+	REQUIRE(rna_copy->seq() == "ACGUGAAAACGU");
+
+	rna["stem/3"]->seq("UUUU");
+
+	REQUIRE(rna.seq() == "ACGUGAAAUUUU");
+	REQUIRE(rna_copy->seq() == "ACGUGAAAACGU");
 }
